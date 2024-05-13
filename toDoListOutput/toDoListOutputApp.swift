12f9822d4049cbd93dc8ee1330c8618edd649570
Alicia <aliciasbao@gmail.com>
2024-05-13 12:52:43 -0400

    Initial Commit

---
+++ toDoListOutput/toDoListOutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListOutputApp.swift
+//  toDoListOutput
+//
+//  Created by Alicia Bao on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoListOutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
