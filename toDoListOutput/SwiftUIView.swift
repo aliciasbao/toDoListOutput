ef6b1194a776e1fa82f97c9a49d7fd4c73bc960c
Alicia <aliciasbao@gmail.com>
2024-06-25 14:09:07 -0400

    test

---
+++ toDoListOutput/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  toDoListOutput
+//
+//  Created by Alicia Bao on 6/25/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
