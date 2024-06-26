7c5271d405a38246bd25360df2d38fd9d0af4159
Alicia <aliciasbao@gmail.com>
2024-06-25 21:03:26 -0400

    deleted extra

--- toDoListOutput/NewToDoView.swift
+++ toDoListOutput/NewToDoView.swift
@@ -6,13 +6,52 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @State private var a = "";
+    @State private var b = true;
+    
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
+    @Binding var showNewTask: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Task title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
+                    .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                  .padding()
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is it important?")
+                    .padding()
+            }
+            Button {
+                addToDo()
+                self.showNewTask = false
+            } label: {
+                Text("Save")
+            }
+        }
+        .padding()
     }
+    
+    func addToDo() {
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
+    
 }
 
 #Preview {
-    NewToDoView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: ToDoItem.self, configurations: config)
+
+    let toDo = ToDoItem(title: "Example ToDo", isImportant: false)
+    return NewToDoView(toDoItem: toDo, showNewTask: .constant(true))
+        .modelContainer(container)
 }
--- toDoListOutput/toDoListOutputApp.swift
+++ toDoListOutput/toDoListOutputApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct toDoListOutputApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: ToDoItem.self)
         }
     }
 }
